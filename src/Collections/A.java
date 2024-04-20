2893e9ea9ab0ac40d0d4f0c79e047a7fba123222
AdarshRai3 <adarshaibusiness@gmail.com>
2024-04-20 14:13:33 +0530

    Learn Enum

---
+++ src/Collections/A.java
@@ -0,0 +1,6 @@
+package Collections;
+
+public interface A {
+    public void hello();
+}
+//interface can only have abstract method which dont have body but we can use default to write method with body in interfaces
---
+++ src/Collections/CollectionEnum.java
@@ -0,0 +1,36 @@
+package Collections;
+
+public class CollectionEnum  {
+    enum Week implements A{
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday;
+
+        Week(){
+            System.out.println("Construntor called for this"+this);
+        }
+        //when we call the Week() method then , this inside it should ponly run for Monday bbut that is not the case it runs for all day that are present
+        //this is not public&protected(enum cannot have children) 
+        //why canno create new object
+        //that is not the enum concept
+        public void hello(){
+            System.out.println("Hello");
+        }
+   };
+    public static void main(String[] args) {
+        Week week = Week.Monday;
+        // for(Week day: Week.values()){
+        //     System.out.println(day);
+        // }this is how we can iterate through enum
+        System.out.println(week);
+        System.out.println(week.ordinal());
+        //week.ordinal method will give us the index/position of enum declaration
+        week.hello();
+        //now we can caled hello()  method in psvm from enum Week by overriding hello() method in enum by implementing A interface.
+    }
+}
+//Enum are group of variable that cannot be changed.
